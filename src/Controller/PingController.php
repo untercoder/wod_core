ed3bd96be20059f13dc91ad8cf1232c6b6fb7464
amokrynskii <amokrynskii@team.amocrm.com>
2023-02-08 16:18:08 +0300

    + docker-compose php81 nginx mysql

---
+++ src/Controller/PingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PingController extends AbstractController
+{
+    #[Route('/')]
+    public function ping(): Response
+    {
+        return $this->json(['response' => 'ok']);
+    }
+
+}
