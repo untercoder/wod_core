fe6391ecf9116e60f7dfe7d2b7d5d019c0686b0c
amokrynskii <amokrynskii@team.amocrm.com>
2023-02-10 16:37:59 +0300

    +telegram bundle env config

--- src/Controller/PingController.php
+++ src/Controller/PingController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Borsaco\TelegramBotApiBundle\Service\Bot;
 
 class PingController extends AbstractController
 {
@@ -12,6 +13,20 @@ class PingController extends AbstractController
     public function ping(): Response
     {
         return $this->json(['response' => 'ok']);
+    }
+
+    #[Route('/check_bot')]
+    public function checkBot(Bot $bot) : Response
+    {
+        $wod_bot = $bot->getBot('wod_dev');
+
+        $wod_bot->sendMessage([
+            'chat_id' => 670407504,
+            'text' => "Hello im work!",
+            'parse_mod' => 'text'
+        ]);
+
+        return $this->json(['user' => $wod_bot->getMe()]);
     }
 
 }
